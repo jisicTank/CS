236e4931c1e428ce2485cc03d6beb5d0420fe1de
socialDe <jaeuk9407@gmail.com>
2021-02-16 00:02:33 +0900

    Create Data Structure

---
+++ Data Structure/code/LinkedList.java
@@ -0,0 +1,95 @@
+package linkedList_2021;
+
+class LinkedList { 
+    Node head;
+  
+    class Node { 
+        int data; 
+        Node next;
+        
+        Node(int d) {
+        	data = d; next = null; 
+        } 
+    } 
+  
+    // 맨 앞에 새로운 데이터 노드를 추가 
+    public void push(int new_data) 
+    { 
+        Node new_node = new Node(new_data); 
+  
+        new_node.next = head; 
+  
+        head = new_node; 
+    } 
+  
+    // 특정 노드 뒤에 데이터 노드를 추가
+    public void insertAfter(Node prev_node, int new_data) 
+    { 
+        if (prev_node == null) 
+        { 
+            System.out.println("The given previous node cannot be null"); 
+            return; 
+        } 
+  
+        Node new_node = new Node(new_data); 
+  
+        new_node.next = prev_node.next; 
+  
+        prev_node.next = new_node; 
+    } 
+
+    // 맨 뒤에 데이터 노드를 추가
+    public void append(int new_data) 
+    { 
+        Node new_node = new Node(new_data); 
+  
+        if (head == null) 
+        { 
+            head = new Node(new_data); 
+            return; 
+        } 
+
+        new_node.next = null; 
+  
+        Node last = head;  
+        while (last.next != null) 
+            last = last.next; 
+  
+        last.next = new_node; 
+        return; 
+    } 
+  
+    public void printList() 
+    { 
+        Node tnode = head; 
+        while (tnode != null) 
+        { 
+            System.out.print(tnode.data+" "); 
+            tnode = tnode.next; 
+        } 
+    } 
+  
+    
+    // ---------------- main ---------------
+    public static void main(String[] args) { 
+        LinkedList llist = new LinkedList(); 
+  
+        //6->NUllist 
+        llist.append(6); 
+        
+        // 7->6->NUllist 
+        llist.push(7); 
+     
+        // 1->7->6->NUllist 
+        llist.push(1); 
+
+        // 1->7->6->4->NUllist 
+        llist.append(4); 
+
+        // 1->7->8->6->4->NUllist 
+        llist.insertAfter(llist.head.next, 8); 
+  
+        System.out.println("\nCreated Linked list is: "); 
+        llist.printList(); 
+    } 
+}
